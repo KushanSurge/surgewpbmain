e9e63b849c129b933dd99fe5e018883e3d9e83e0
snntx <krekanayake@gmail.com>
2026-03-24 14:23:35 +0530

    Release 2.1

--- surgewpbmain.php
+++ surgewpbmain.php
@@ -3,7 +3,7 @@
  * Plugin Name: SurgeWP Boilerplate
  * Plugin URI:  https://surge.global
  * Description: A modular, shortcode-based WordPress plugin boilerplate.
- * Version:     2.0
+ * Version:     2.1
  * Author:      Surge Global
  * Text Domain: surgewpb
  * Domain Path: /languages
